429448323fa99f2befbec2211c06c0ac091437b3
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2025-12-29 20:13:22 +0000

    6

--- backend.py
+++ backend.py
@@ -28,15 +28,15 @@ import os
 #                                                                                                                   #
 #####################################################################################################################
 ### Web Researcher API, Model and Tool ###
-openai_api_key = os.environ["OPENAI_API_KEY"]
-serpapi_api_key= os.environ["SERPAPI_API_KEY"]
+'''openai_api_key = os.environ["OPENAI_API_KEY"]
+serpapi_api_key= os.environ["SERPAPI_API_KEY"]'''
 
-search_api = SerpAPIWrapper(serpapi_api_key= serpapi_api_key)
-researcher_llm = ChatOpenAI(api_key=openai_api_key,
+search_api = SerpAPIWrapper()
+researcher_llm = ChatOpenAI(
     model = 'gpt-5.1',
     temperature= 0
 )
-reporter_llm = ChatOpenAI(api_key=openai_api_key,
+reporter_llm = ChatOpenAI(
     model = 'gpt-5.1',
     temperature= 0.2
 )
